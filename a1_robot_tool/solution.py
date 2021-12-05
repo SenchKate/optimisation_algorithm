0a92403c5261e558112dde7f1526b3827cc5e933
SenchKate <42967906+SenchKate@users.noreply.github.com>
2021-12-05 21:06:54 +0100

    solution added

--- a1_robot_tool/solution.py
+++ a1_robot_tool/solution.py
@@ -19,6 +19,9 @@ class RobotTool(MathematicalProgram):
         pr: 1-D np.array
         l: float
         """
+        self.q0 = q0
+        self.pr= pr
+        self.l = l
         # in case you want to initialize some class members or so...
 
     def evaluate(self, x):
@@ -27,15 +30,24 @@ class RobotTool(MathematicalProgram):
         ----
         MathematicalProgram.evaluate
         """
-
+        pb = np.zeros((2,))
+        pb[0] = math.cos(x[0]) + 1/2*math.cos(x[0]+x[1])+1/3*math.cos(x[0]+x[1]+x[2])
+        pb[1] = math.sin(x[0]) + 1/2*math.sin(x[0]+x[1])+1/3*math.sin(x[0]+x[1]+x[2])
+        y =np.sqrt((pb-self.pr).T@(pb-self.pr)+self.l*((x- self.q0).T@(x- self.q0)))
         # add the main code here! E.g. define methods to compute value y and Jacobian J
         # y = ...
-        # J = ...
+        J = np.zeros((2,3))
 
+        J[0,0] = (pb[0]-self.pr[0])*2*(-math.sin(x[0])-1/2*math.sin(x[0]+x[1])-math.sin(x[0]+x[1]+x[2]))+self.l*2*(x[0]-self.q0[0])
+        J[0,1] = (pb[0]-self.pr[0])*2*(-1/2*math.sin(x[0]+x[1])-math.sin(x[0]+x[1]+x[2]))+self.l*2*(x[1]-self.q0[1])
+        J[0,2] = (pb[0]-self.pr[0])*2*(-math.sin(x[0]+x[1]+x[2]))+self.l*2*(x[2]-self.q0[2])
+        J[1,0] = (pb[1]-self.pr[1])*2*(math.cos(x[0]) + 1/2*math.cos(x[0]+x[1]) + 1/2*math.cos(x[0]+x[1]+x[2]))+self.l*2*(x[0]-self.q0[0])
+        J[1,1] = (pb[1]-self.pr[1])*2*(1/2*math.cos(x[0]+x[1]) + 1/2*math.cos(x[0]+x[1]+x[2]))+self.l*2*(x[1]-self.q0[1])
+        J[1,2] = (pb[1]-self.pr[1])*2*(1/2*math.cos(x[0]+x[1]+x[2]))+self.l*2*(x[2]-self.q0[2])
         # y is a 1-D np.array of dimension m
         # J is a 2-D np.array of dimensions (m,n)
         # where m is the number of features and n is dimension of x
-        # return  y  , J
+        return  np.array([y]).reshape((1,1))  , J
 
     def getDimension(self):
         """
